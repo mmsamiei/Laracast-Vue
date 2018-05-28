2e6bbb046bde0f0a26c10fd2b8dc8197f8a158ac
mmsamiei <mohmahsamiei@gmail.com>
2018-05-28 14:05:11 +0430

    add 16

---
+++ 16/my-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Message>Hello There</Message>
+    <Message>Hello There</Message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
